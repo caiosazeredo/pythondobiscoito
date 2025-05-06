325a280423fa969626a0adc06141b10500d9f837
Caio Azeredo <caiosazeredo@cos.ufrj.br>
2025-05-06 01:27:47 -0300

    configurações

---
+++ config.py
@@ -0,0 +1,37 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'chave-secreta-padrao-mudar-em-producao'
+    
+    # Configuração do MySQL com as credenciais fornecidas
+    MYSQL_HOST = os.environ.get('MYSQL_HOST')
+    MYSQL_PORT = os.environ.get('MYSQL_PORT')
+    MYSQL_USER = os.environ.get('MYSQL_USER')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
+    MYSQL_DB = os.environ.get('MYSQL_DB')
+    
+    # String de conexão SQLAlchemy configurada corretamente
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+    
+    # Configuração SSL adequada
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {
+            "ssl": {
+                "ssl_mode": "REQUIRED"
+            }
+        }
+    }
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # Configurações de e-mail
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SUBJECT = os.environ.get('MAIL_DEFAULT_SUBJECT')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
